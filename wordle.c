1e83da5174637c965512adace5c5a2ed34d2620b
klee33 <klee33@uw.edu>
2022-03-03 13:35:22 -0800

    finished game behavior

--- wordle.c
+++ wordle.c
@@ -17,8 +17,9 @@
 void print_error(char* err_msg);
 
 // prints wordle hint for current guess
-// (grey = not in word, yellow = elsewhere, green = correct position)
-void print_hints(char* answer, char* guess);
+// (grey = not in word, yellow = elsewhere, green = correct position).
+// Returns true if correct word is guessed, and false otherwise
+bool print_hints(char* answer, char* guess);
 
 // private helper for print_hints()
 // (hint_color is 2 for green,
@@ -34,90 +35,112 @@ int main(int argc, char** argv) {
   // load all possible answer words from list into memory
   char** answer_words = answer_words_list();
 
-  // game loop
-  while (1) {
-    // pick random word from answer words list
-    // NOTE: not completely uniform distribution
-    srand(time(NULL));
-    int w = rand() % ANSWER_WORDS_COUNT;
-    char* word = answer_words[w];
+  // pick random word from answer words list
+  // NOTE: not completely uniform distribution
+  srand(time(NULL));
+  int w = rand() % ANSWER_WORDS_COUNT;
+  char* word = answer_words[w];
+
+  // clear terminal for new game
+  system("clear");
+
+  // explain game
+  printf("You have 6 tries to guess a randomly chosen 5-letter English word!\n");
+
+  // for debugging    
+  // printf("Actual word: %s", word);
+
+  // user guess loop
+  int guesses_used = 0;
+  while (guesses_used < GUESS_LIMIT) {
+    printf("\n");
+    printf("Guess %d:\n", guesses_used + 1);
         
-    // printf("Actual word: %s", word);
+    // get user guess
+    char curr_guess[100];
+    if (fgets(curr_guess, sizeof(curr_guess), stdin) == NULL) {
+      print_error("Invalid input, please retry\n");
+      continue;
+    }
 
-    // user guess loop
-    int guesses_used = 0;
-    while (guesses_used < GUESS_LIMIT) {
-      printf("\n");
-      printf("Guess %d:\n", guesses_used + 1);
-          
-      // get user guess
-      char curr_guess[100];
-      if (fgets(curr_guess, sizeof(curr_guess), stdin) == NULL) {
-        print_error("Invalid input, please retry\n");
-        continue;
+    // remove new line character
+    size_t len = strlen(curr_guess);
+    if (len > 0 && curr_guess[len - 1] == '\n') {
+      curr_guess[len - 1] = '\0';
+    }
+
+    // check validity of word
+    // (5 letters long, alphabetic)
+    int letter_count = 0;
+    bool alphabetic = true;
+    for (char* i = curr_guess; *i != '\0'; i++) {
+      if (!(*i >= 65 && *i <= 90) && !(*i >= 97 && *i <= 122)) {
+        // not alphabetic ASCII
+        alphabetic = false;
+        break;
       }
 
-      // remove new line character
-      size_t len = strlen(curr_guess);
-      if (len > 0 && curr_guess[len - 1] == '\n') {
-        curr_guess[len - 1] = '\0';
-      }
+      letter_count++;
+    }
+    if (!alphabetic) {
+      print_error("Non-alphabetic characters entered\n");
+      continue;
+    } else if (letter_count > WORD_LENGTH) {
+      print_error("Word too long, must enter 5 letters exactly\n");
+      continue;
+    } else if (letter_count < WORD_LENGTH) {
+      print_error("Word too short, must enter 5 letter exactly\n");
+      continue;
+    }
 
-      // check validity of word
-      // (5 letters long, alphabetic)
-      int letter_count = 0;
-      bool alphabetic = true;
-      for (char* i = curr_guess; *i != '\0'; i++) {
-        if (!(*i >= 65 && *i <= 90) && !(*i >= 97 && *i <= 122)) {
-          // not alphabetic ASCII
-          alphabetic = false;
-          break;
-        }
+    // convert guess to all lowercase
+    for (int i = 0; i < 5; i++) {
+      curr_guess[i] = tolower(curr_guess[i]);
+    }
 
-        letter_count++;
+    // check if word is a real English word from word lists
+    bool real_word = false;
+    for (int i = 0; i < ANSWER_WORDS_COUNT; i++) {
+      if (strcmp(answer_words[i], curr_guess) == 0) {
+        real_word = true;
+        break;
       }
-      if (!alphabetic) {
-        print_error("Non-alphabetic characters entered\n");
-        continue;
-      } else if (letter_count > WORD_LENGTH) {
-        print_error("Word too long, must enter 5 letters exactly\n");
-        continue;
-      } else if (letter_count < WORD_LENGTH) {
-        print_error("Word too short, must enter 5 letter exactly\n");
-        continue;
-      }
-
-      // convert guess to all lowercase
-      for (int i = 0; i < 5; i++) {
-        curr_guess[i] = tolower(curr_guess[i]);
-      }
-
-      // check if word is a real English word from word lists
-      bool real_word = false;
-      for (int i = 0; i < ANSWER_WORDS_COUNT; i++) {
-        if (strcmp(answer_words[i], curr_guess) == 0) {
+    }
+    if (!real_word) {
+      for (int i = 0; i < GUESSABLE_WORDS_COUNT; i++) {
+        if (strcmp(guessable_words[i], curr_guess) == 0) {
           real_word = true;
           break;
         }
       }
-      if (!real_word) {
-        for (int i = 0; i < GUESSABLE_WORDS_COUNT; i++) {
-          if (strcmp(guessable_words[i], curr_guess) == 0) {
-            real_word = true;
-            break;
-          }
-        }
-      }
-      if (!real_word) {
-        print_error("Not a real English word\n");
-        continue;
-      }
-
-      // print out hints from guess
-      print_hints(word, curr_guess);
-
-      guesses_used++;
     }
+    if (!real_word) {
+      print_error("Not a real English word\n");
+      continue;
+    }
+
+    // print out hints from guess
+    bool won = print_hints(word, curr_guess);
+
+    // check if correct word is guessed
+    if (won) {
+      printf("\n");
+      printf("Congratulations, you have guessed the correct word in ");
+      if (guesses_used + 1 == 1) {
+        printf("%d guess!\n", guesses_used + 1);
+      } else {
+        printf("%d guesses!\n", guesses_used + 1);
+      }
+
+      break;
+    }
+
+    guesses_used++;
+  }
+
+  if (guesses_used == 6) {
+    printf("\n");
+    print_error("Unfortunately, you have run out of guesses! Better luck next time.\n");
   }
 
   // free all dynamically allocated data before exiting
@@ -133,7 +156,7 @@ void print_error(char* err_msg) {
   printf("\033[0m");
 }
 
-void print_hints(char* answer, char* guess) {
+bool print_hints(char* answer, char* guess) {
   printf("\n");
 
   // for printing out hint
@@ -146,9 +169,12 @@ void print_hints(char* answer, char* guess) {
     ans_letter_count[answer[i] - 'a']++;
   }
 
-  // logic for designating hints
+  // logic for designating hints and counting
+  // correct letters
+  int correct_letters = 0;
   for (int i = 0; i < WORD_LENGTH; i++) {
     if (answer[i] == guess[i]) {
+      correct_letters++;
       hint[i] = 2;
       ans_letter_count[guess[i] - 'a']--;
       continue;
@@ -183,6 +209,9 @@ void print_hints(char* answer, char* guess) {
 
   printf("\033[0m");
   printf("\n");
+
+  // check if answer was guessed
+  return (correct_letters == 5);
 }
 
 void set_hint_color(int hint_color) {
